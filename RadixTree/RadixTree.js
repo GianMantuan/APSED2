ca8488282bb61adca50dc8e910ca635398fbb696
gianmantuan <gian.ti@grupointegrado.br>
2018-11-23 09:11:47 -0200

    Add Comentarios

--- RadixTree/RadixTree.js
+++ RadixTree/RadixTree.js
@@ -5,15 +5,13 @@ class RadixTree {
     constructor(dict = []){
         this._dict = dict
         this.root = new Node();
-
+        
+        //A Arvore é instânciada recebendo um array e automaticamente chama o metodo build
         this.build(dict)
     }
 
-    getRoot(){
-        return this.root
-    }
-
     build(dict){
+        //Para a chave do array ele itera sobre cada index chamando o metodo _add
         for (let key in dict) {
             let tokens = dict[key]            
 
@@ -23,12 +21,8 @@ class RadixTree {
         }
     }
 
-    insert(str) {
-        this._list.push(str)
-        this._add(str, this._list.length - 1)
-    }
-
     _diff(strA, strB) {
+        // A diferença retorna a posição (index) de quando as duas strings nao forem iguais
         let length_A = (strA || '').length
         let length_B = (strB || '').length
         let total_len = length_A < length_B ? length_A : length_B
@@ -44,12 +38,15 @@ class RadixTree {
     }
 
     _add(str, parent) {
+        // Para diferenciar se é String ou Objeto, ele verifica o tipo do item que esta sendo passado
+        // A string entraria como parte do Dicionário e o Objeto como parte da Agenda
         let val = str
         if (typeof str === 'object') {
             val = str.Nome
         }
 
         let node = parent || this.root
+        // Após receber um nó pai ou a raiz ele verifica seus vertices para encontrar similaridade e diferença
 
         for (let edge of node.edges) {
             let diff_index = this._diff(val, edge.label)
@@ -60,22 +57,29 @@ class RadixTree {
                     return
                 }
                 
+                //Nesse momento ele cria um array de vértices
+                // O primeiro contendo o vertice antigo ate o index que foi retornado da função _diff
+                // O segundo vertice contém a parte da palavra/objeto que veio da função build, também a partir do index
+
                 let edges = [
                     new Edge(edge.label.slice(diff_index), edge.targetNode), 
                     new Edge(val.slice(diff_index), new Node([str]))
                 ]
+
                 let new_node = new Node()
                 new_node.addEdges(edges)
 
-                node.removeEdge(edge)
-                node.addEdge(new Edge(val.slice(0, diff_index), new_node))
+                node.removeEdge(edge) // Remove o vértice antigo
+                node.addEdge(new Edge(val.slice(0, diff_index), new_node)) // Adiciona o novo array de vertices
                 return
             }
         }
-        
+        // Caso nao tenha nenhuma similaridade com qualquer um dos vertices ja adicionados na arvore
+        // ele simplesmente cria um novo vertice 
         let edge = new Edge(val, new Node([str]))
         node.addEdge(edge)
 
+        // Para facilitar a impressao de dados, o valor final da palavra/objeto é adicionado em um atributo do nó final
         if (node.isLeaf()) {
             node.addEdge('', new Node(node.targetNode.str))
         }
@@ -83,6 +87,8 @@ class RadixTree {
     }
 
     find(prefix, parent) {
+        // Para a busca, ele verifica se o prefixo passado tem alguma similaridade com o algum dos vertices
+        // descendo a arvoce até encontrar o valor final armazenado no atributo 'vals'
         let node = parent || this.root
 
         if (node.isLeaf() || prefix === "") {
